b57430a71c017090b38c7687e83a494a54e72c5e
denizgencay <denizgencay35@gmail.com>
2022-09-19 14:22:58 +0300

    Post, delete methods implemented. Exception handling made.

--- src/main/java/com/example/res/webservices/restfulwebservices/UserDaoService.java
+++ src/main/java/com/example/res/webservices/restfulwebservices/UserDaoService.java
@@ -9,21 +9,33 @@ import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
-    private static List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
 
     static{
-        users.add(new User(1, "Deniz", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Ibraham", LocalDate.now().minusYears(35)));
-        users.add(new User(3, "Cem", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Deniz", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Ibraham", LocalDate.now().minusYears(35)));
+        users.add(new User(++usersCount, "Cem", LocalDate.now().minusYears(20)));
     }
 
     public List<User> findAll(){
         return users;
     }
 
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
     public User findOne(int id){
         Predicate<? super User> predicate = user -> user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id){
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
     }
 
 }
--- src/main/java/com/example/res/webservices/restfulwebservices/UserResource.java
+++ src/main/java/com/example/res/webservices/restfulwebservices/UserResource.java
@@ -1,9 +1,12 @@
 package com.example.res.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,8 +25,47 @@ public class UserResource {
 
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return userDaoService.findOne(id);
+        User user = userDaoService.findOne(id);
+        if (user == null) throw new UserNotFoundException("id: " + id);
+        return user;
+    }
 
+    @DeleteMapping(path = "/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        userDaoService.deleteById(id);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
